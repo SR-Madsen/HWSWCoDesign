c97160d4807c3493cf1934042a2ec5a7cb94ec97
SR-Madsen <sebastianrudmadsen@gmail.com>
2022-06-15 10:11:43 +0200

    Minor assignment changes. Ran simulations again.

--- Assignment2/Assignment2.srcs/sources_1/bd/design_1/sim/design_1.vhd
+++ Assignment2/Assignment2.srcs/sources_1/bd/design_1/sim/design_1.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2020.1 (lin64) Build 2902540 Wed May 27 19:54:35 MDT 2020
---Date        : Thu Jun  9 10:24:03 2022
+--Date        : Wed Jun 15 08:21:30 2022
 --Host        : Ubuntu-ZBook-15-G2 running 64-bit Ubuntu 18.04.6 LTS
 --Command     : generate_target design_1.bd
 --Design      : design_1
